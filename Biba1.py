88921bdfee109e5aab37df3ce28d513b4ac775ed
Margarita Kononovich <3826210@gmail.com>
2025-02-02 20:19:42 +0100

    Memory!!!!!!

--- Biba1.py
+++ Biba1.py
@@ -1,1 +1,11 @@
 print ("Hello Maska")
+
+
+Create new brunch
+make changes
+commit
+push
+create a pull request from my brunch to main
+merge pull request - обновление ветки main на сервере
+checkout main brunch
+update (pull) - забрали изменения main ветки с сервера
