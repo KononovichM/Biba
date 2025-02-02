c0407eb39913939bb5f3589753802c12df53e1b7
Margarita Kononovich <3826210@gmail.com>
2025-02-02 22:44:34 +0100

    -----

--- Biba1.py
+++ Biba1.py
@@ -9,3 +9,5 @@ create a pull request from my brunch to main
 merge pull request - обновление ветки main на сервере
 checkout main brunch
 update (pull) - забрали изменения main ветки с сервера
+
+------
